3e8703da32378bb92c30cdd4e6e714176d307660
Tushar <tusharjana.aj3@gmail.com>
2025-04-04 23:58:11 +0530

    Made some changes in carts

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.http import HttpResponse
 
@@ -14,6 +14,19 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id) #get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                 variation = Variation.objects.get(product=product,variation_category__iexact=key, variation_value__iexact=value)
+                 product_variation.append(variation)
+            except:
+                pass
+
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
@@ -24,6 +37,9 @@ def add_cart(request, product_id):
 
     try:
         cart_item=CartItem.objects.get(product=product,cart=cart)
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -32,6 +48,9 @@ def add_cart(request, product_id):
             quantity=1,
             cart=cart,
         )
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.save()
     return redirect('cart')
 
