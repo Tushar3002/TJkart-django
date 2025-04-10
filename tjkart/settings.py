6ee573a908772c699b1e472281d3c68a92045d7e
Tushar <tusharjana.aj3@gmail.com>
2025-04-10 11:21:44 +0530

    Django Authentication

--- tjkart/settings.py
+++ tjkart/settings.py
@@ -145,3 +145,22 @@ STATICFILES_DIRS = [
 #media files congigure
 MEDIA_URL  ='/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dancerjonathan5@gmail.com'
+EMAIL_HOST_PASSWORD = 'bwslrhszkghiugjx'
+EMAIL_USE_TLS = True
+
+# EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+# EMAIL_HOST_USER = '570f033405e1fc'
+# EMAIL_HOST_PASSWORD = '****601f'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
